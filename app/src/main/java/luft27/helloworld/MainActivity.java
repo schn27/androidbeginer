738024ec04a6d9bf07f121bafdce7c3a4eaaf8ec
Aleksandr Malikov <schn27@gmail.com>
2016-06-15 23:59:06 +0300

    upload

---
+++ app/src/main/java/luft27/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package luft27.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onOk(View view) {
+        EditText name = (EditText) findViewById(R.id.name);
+        TextView message = (TextView) findViewById(R.id.message);
+
+        message.setText("Hello, " + name.getText());
+    }
+}
